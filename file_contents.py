5b409315acbe790f3a158c54c42f6cd3b393ae31
ngoc.tran <ngoc.tran@hope.edu>
2023-01-29 16:53:28 -0500

    Add files

--- file_contents.py
+++ file_contents.py
@@ -1,5 +1,15 @@
 import sys
+import subprocess
 # Arguments passed
 print("\nName of Python script:", sys.argv[0])
  
 print("Root file name: ", sys.argv[1]) 
+print("")
+
+# Main program
+# name of the file: tree_sha.py
+cmd = ('git', 'log','--pretty', '--',sys.argv[1], '-1') #use another command
+process = subprocess.run(cmd, capture_output=True, text=True)
+
+info = process.stdout
+print(info)
--- tree_sha.py
+++ tree_sha.py
@@ -6,12 +6,14 @@
 import sys
 import subprocess
 
- 
-# Arguments passed
-print("\nName of Python script:", sys.argv[0])
+
      
 #main program:
-print("SHA-1 code: ", sys.argv[1]) #SHA-1: 3b54452c683ab37a1b565f0b559f6e26e1df79d9
+#SHA-1: 3b54452c683ab37a1b565f0b559f6e26e1df79d9
 cmd = ('git', 'cat-file','-p',sys.argv[1]) #use another command
 process = subprocess.run(cmd, capture_output=True, text=True)
-print(process.stdout)
+
+info = process.stdout
+
+treeCode = info.split("\n")[0][5:] #The first line is tree information, substring staring from index 5 is the sha1 code
+print(treeCode)
