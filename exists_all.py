c08cab374ae7cf95bc458b8c9ba2876cc027cb91
Annie <ngoc.tran@hope.edu>
2023-03-07 12:03:43 -0500

    Revise file_contents.py

--- exists_all.py
+++ exists_all.py
@@ -3,8 +3,6 @@ import subprocess
 import os
 #main program:
 #SHA-1: 3b54452c683ab37a1b565f0b559f6e26e1df79d9
-# cmd = ('git', 'cat-file','-p',sys.argv[1]) #use another command
-# process = subprocess.run(cmd, capture_output=True, text=True)
 
 #SHA-1 commit: 3b54452c683ab37a1 - tree_sha.py
 #sha1 commit: 7470e0fcaa786087af2849be1 - aFile.txt
@@ -16,11 +14,7 @@ if os.path.exists(fileN):
     else:
 
         for argv in sys.argv[2:]:
-            # print(argv)
-            # cmd = ('git', 'log','--name-only','--',sys.argv[1]) #use another command
-            # process = subprocess.run(cmd, capture_output=True, text=True)
-            # treeCode = process.stdout.split("\n")
-            #cmd = ('git', 'log','--name-only','--',sys.argv[1]) #use another command
+
             cmd = ('git', 'cat-file', '-t', argv)
             process = subprocess.run(cmd, capture_output=True, text=True)
             cmdType = process.stdout
@@ -39,15 +33,14 @@ if os.path.exists(fileN):
             infoList = list(filter(None, process.stdout.split("\n")))
             isValid = False
             for info in infoList:
-                # print(info)
+
                 infoArr = list(filter(None, info.split()))
-                # print(infoArr)
-                # print(infoArr[0])
+
                 fName = infoArr[3]
                 if fName == fileN:
                     isValid = True
                     break
-            # print('')
+
 
         if not isValid:
             print('False')
--- file_contents.py
+++ file_contents.py
@@ -7,43 +7,49 @@ fileN = sys.argv[1]
 if os.path.exists(fileN):
     if os.path.isdir(fileN):
         print(fileN, 'is not a file but a directory')
-    else:  
-        cmd = ('git', 'log','-n','1', '--',fileN) #use another command
+    else: 
+        script_dir = os.path.dirname(__file__)
+        #get the current branch in HEAD
+        with open(os.path.join(script_dir, '.git/HEAD')) as f:
+            
+            currentBranch = list(filter(None, f.readlines()[0].split("/")))[-1][:-1]
+        re_path = '.git/refs/heads/' + currentBranch
+        abs_file_path = os.path.join(script_dir, re_path)
+        with open(abs_file_path) as f: 
+            sha1 = f.readlines()[0][0:-1] #get the sha1 of the last commit in .git/refs/heads/currentbranch and exclude the new line character
+
+        #Use git cat-file to get the info of the last commit 
+        cmd = ('git', 'cat-file','-p',sha1) 
         process = subprocess.run(cmd, capture_output=True, text=True)
 
         info = process.stdout
 
-        #print(info)
-
+        #Get the author info
         arr = list(filter(None, info.split("\n")))
-        author = arr[1]
+        authorInfo = list(filter(None, arr[2].split()))
 
-        print(author)
+        print('Author:', authorInfo[1], authorInfo[2])
         print("")
-
-        shaCommit = arr[0][7:]
-
+        #get the tree sha
+        shaCommit = arr[0][5:]
+        #get the list of blob sha
         cmd = ('git', 'cat-file','-p',shaCommit) 
         process = subprocess.run(cmd, capture_output=True, text=True)
 
-        shaTree = list(filter(None,process.stdout.split("\n")))[0][5:]
-        cmd = ('git', 'cat-file','-p',shaTree) 
-        process = subprocess.run(cmd, capture_output=True, text=True)
-
         infoList = list(filter(None, process.stdout.split("\n")))
-
+        #get the sha associated with the specified file
+        fileSha1 = ""
         for info in infoList:
             if info.split()[3] == fileN:
-                print("SHA-1 for", fileN, list(filter(None,info.split()))[2])
+                fileSha1 = list(filter(None,info.split()))[2]
+                print("SHA-1 for", fileN, ':',fileSha1)
                 break
 
-        print("")
+        print("")   
+        #get the content of the file     
+        cmd = ('git', 'cat-file','-p',fileSha1) 
+        process = subprocess.run(cmd, capture_output=True, text=True)
+        print(process.stdout)
 
-        with open(sys.argv[1]) as f:
-            lines = f.readlines()
-
-
-        for line in lines:
-            print(line)
 else:
     print('File does not exist')
